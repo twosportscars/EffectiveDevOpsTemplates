eda4d7e38c510a3b3ee24d3d6da5994e730d91df
twosportscars <pgjohnso1@verizon.net>
2021-02-10 04:12:50 +0000

    Adding helloworld Troposphere template
    Adding helloworld Troposphere template"

---
+++ helloworld-cf-template.py
@@ -0,0 +1,97 @@
+"""Generating CloudFormation template."""
+
+from troposphere import (
+    Base64,
+    ec2,
+    GetAtt,
+    Join,
+    Output,
+    Parameter,
+    Ref,
+    Template,
+)
+
+from troposphere.ec2 import NetworkInterfaceProperty
+
+ApplicationPort = "3000"
+
+t = Template()
+
+t.add_description("Effective DevOps in AWS: HelloWorld web application")
+
+t.add_parameter(Parameter(
+    "KeyPair",
+    Description="Name of an existing EC2 KeyPair to SSH",
+    Type="AWS::EC2::KeyPair::KeyName",
+    ConstraintDescription="must be the name of an existing EC2 KeyPair.",
+))
+
+t.add_resource(ec2.SecurityGroup(
+    "SecurityGroup",
+    GroupDescription="Allow SSH and TCP/{} access".format(ApplicationPort),
+    SecurityGroupIngress=[
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort="22",
+            ToPort="22",
+            CidrIp="172.30.2.5/32",
+        ),
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort=ApplicationPort,
+            ToPort=ApplicationPort,
+            CidrIp="0.0.0.0/0",
+        ),
+    ],
+    VpcId="vpc-1cf07e78",
+))
+
+ud = Base64(Join('\n', [
+    "#!/bin/bash",
+    "sudo yum install --enablerepo=epel -y nodejs",
+    "wget http://bit.ly/2vESNuc -O /home/ec2-user/helloworld.js",
+    "wget http://bit.ly/2vVvT18 -O /etc/init/helloworld.conf",
+    "start helloworld"
+]))
+
+t.add_resource(ec2.Instance(
+    "instance",
+    ImageId="ami-0e90493c90abc1f3b",
+    InstanceType="t4g.micro",
+    KeyName=Ref("KeyPair"),
+    NetworkInterfaces=[
+        NetworkInterfaceProperty(
+            GroupSet=[
+                Ref("SecurityGroup")],
+            AssociatePublicIpAddress = 'true',
+            DeviceIndex='0',
+            DeleteOnTermination='true',
+        SubnetId="subnet-9a01c4ec")],
+#     UserData=ud,
+))
+
+t.add_output(Output(
+    "InstancePublicIp",
+    Description="Public IP of our instance.",
+    Value=GetAtt("instance", "PublicIp"),
+))
+
+t.add_output(Output(
+    "WebUrl",
+    Description="Application endpoint",
+    Value=Join("", [
+        "http://", GetAtt("instance", "PublicDnsName"),
+        ":", ApplicationPort
+    ]),
+))
+
+print (t.to_json())
+
+
+
+
+
+
+
+
+
